48e1011324a50c974eb4fe8c840085069eac49e8
Kabeer Kashif <abdulkabeerkashif9@gmail.com>
2026-02-22 10:18:51 +0000

    V2

--- erpnext_login_theme/hooks.py
+++ erpnext_login_theme/hooks.py
@@ -1,12 +1,12 @@
 app_name = "erpnext_login_theme"
-app_title = "ERPNext Login Theme"
-app_publisher = "Open Source"
-app_description = "Corporate theme for ERPNext"
+app_title = "HM Technologies ERP"
+app_publisher = "HM Technologies"
+app_description = "HM Technologies White-Label ERP"
 app_icon = "octicon octicon-paintcan"
-app_color = "#1a56db"
+app_color = "#2a9d8f"
 app_email = ""
 app_license = "MIT"
 app_version = "1.0.0"
 
 website_route_rules = [{"from_route": "/login", "to_route": "login"}]
-app_include_css = "/assets/frappe/css/erpnext_theme.css"
+app_include_css = "/assets/frappe/css/hm_theme.css"
